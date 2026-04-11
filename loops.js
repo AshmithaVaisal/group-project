ffe1e59f3a4dff48fee18f2069c667852db68354
rajeshdiny <rajeshraja.r98@gmail.com>
2026-04-11 21:29:35 +0530

    day2 changes in loop file

--- loops.js
+++ loops.js
@@ -1,4 +1,4 @@
-for (let i = 1; i <= 5; i++) {
+for (let i = 1; i <= 15; i++) {
     console.log("Number:", i);
 }
 
@@ -8,3 +8,19 @@ do {
     console.log("Count:", i);
     i++;
 } while (i <= 5);
+
+// users.js
+
+const users = [
+    { name: "Raj", age: 25 },
+    { name: "Amit", age: 17 },
+    { name: "Priya", age: 30 },
+    { name: "John", age: 15 }
+];
+
+// Loop and print only adults (age >= 18)
+for (let i = 0; i < users.length; i++) {
+    if (users[i].age >= 18) {
+        console.log("Adult User:", users[i].name);
+    }
+}
